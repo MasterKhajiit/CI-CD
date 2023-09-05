de2ed7238bf3c939dc413c68d5d38647a263971a
Wadim Rudkovski <my@temp.com>
2023-09-05 17:00:57 +0300

    header test

--- info.sh
+++ info.sh
@@ -5,27 +5,27 @@ mkdir html
 date > html/index.html
 
 #show kernel data
-echo "-------------------- Hostname info --------------------" >> html/index.html
+echo "<h2>-------------------- Hostname info --------------------</h2>" >> html/index.html
 hostnamectl >> html/index.html
 
 #show ip data
-echo "-------------------- Ip info --------------------" >> html/index.html
+echo "<h2>-------------------- Ip info --------------------</h2>" >> html/index.html
 ip a >> html/index.html
 
 #show CPU info
-echo "-------------------- Processor info --------------------" >> html/index.html
+echo "<h2>-------------------- Processor info --------------------</h2>" >> html/index.html
 cat /proc/cpuinfo >> html/index.html 
 
 #show memory usage
-echo "-------------------- RAM info --------------------" >> html/index.html
+echo "<h2>-------------------- RAM info --------------------</h2>" >> html/index.html
 free -m >> html/index.html
 
 #show hard drive data
-echo "-------------------- HDD info --------------------" >> html/index.html
+echo "<h2>-------------------- HDD info --------------------</h2>" >> html/index.html
 df -h >> html/index.html
 
 #show PCI Devices
-echo "-------------------- PCI info --------------------" >> html/index.html
+echo "<h2>-------------------- PCI info --------------------</h2>" >> html/index.html
 lspci >> html/index.html
 
 #test webhook
